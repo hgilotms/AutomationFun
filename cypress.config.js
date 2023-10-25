b3bdb40d473c0cae0d76e1905fa2b845fe672608
Hendy Gilot <86656539+hgilotms@users.noreply.github.com>
2023-10-24 20:30:41 -0400

    cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://assignment--lennar.netlify.app/',
-    "defaultCommandTimeout": 45000,
-    "pageLoadTimeout": 45000,
+    "defaultCommandTimeout": 90000,
+    "pageLoadTimeout": 90000,
     "experimentalStudio": true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
